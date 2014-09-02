37832f50610bd100bf07ab639e4ff3c4b09a5bae
Emil Nilsson <eonilsson@gmail.com>
2014-09-02 18:52:20 +0200

    Implement a basic version of tee in ch04

---
+++ ch04/tee/tee.c
@@ -0,0 +1,77 @@
+// for fprintf, stdin, stderr
+#include <stdio.h>
+// for open, close, O_WRONLY, O_CREAT
+#include <fcntl.h>
+// for read, STDIN_FILENO
+#include <unistd.h>
+// for S_IRUSR, S_IWUSER, S_IRGRP, S_IWGRP
+#include <sys/stat.h>
+// for exit, EXIT_SUCCESS, EXIT_FAILURE
+#include <stdlib.h>
+// for errno
+#include <errno.h>
+
+/** Size of the read buffer. */
+#define BUFSIZE 1024
+
+/**
+ * Prints the latest error and quit.
+ */
+void __attribute__((__noreturn__)) error()
+{
+	if (errno) {
+		perror("tee");
+	} else {
+		fprintf(stderr, "Unknown error\n");
+	}
+	exit(EXIT_FAILURE);
+}
+
+int main(int argc, char * argv[])
+{
+	int src_fd = -1, dest_fd = -1;
+	int num_read;
+	char buf[BUFSIZE];
+
+	// check arguments
+	if (argc != 2) {
+		fprintf(stderr, "Usage: tee file\n");
+		return EXIT_FAILURE;
+	}
+
+	src_fd = STDIN_FILENO;
+	dest_fd = open(argv[1], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+	if (dest_fd == -1) {
+		goto fail;
+	}
+
+	while ((num_read = read(src_fd, buf, BUFSIZE-1)) > 0) {
+		// write to destination file
+		if (write(dest_fd, buf, num_read) != num_read) {
+			goto fail;
+		}
+
+		// insert missing end-of-string
+		buf[num_read] = '\0';
+		printf("%s", buf);
+	}
+
+	if (num_read == -1) {
+		goto fail;
+	}
+
+	if (close(dest_fd)) {
+		// couldn't close file, simply mark it as closed
+		dest_fd = -1;
+		goto fail;
+	}
+
+	return EXIT_SUCCESS;
+
+fail:
+	if (dest_fd != -1) {
+		close(dest_fd);
+	}
+	error();
+}
+

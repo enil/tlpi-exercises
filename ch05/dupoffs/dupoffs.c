0d4f66ba937a83424d0085088873280aca863822
Emil Nilsson <eonilsson@gmail.com>
2014-09-06 12:46:06 +0200

    Add checks for read and write in dupoffs in ch05
    Implement checks for moving the offset by calling read and write on file
    descriptors and run mulitple checks on different file descriptors.

--- ch05/dupoffs/dupoffs.c
+++ ch05/dupoffs/dupoffs.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 // for close, unlink, dup, lseek, SEEK_SET, SEEK_CUR, SEEK_END
 #include <unistd.h>
+// for true, false
+#include <stdbool.h>
 // for assert
 #include <assert.h>
 // for errno
@@ -102,31 +104,105 @@ static int close_fds(int fds[], size_t count)
 }
 
 /**
+ * Check that all file descriptors have the same offset.
+ */
+static int check_offsets(int fds[], size_t count, off_t expected_offset)
+{
+	int failed = 0;
+
+	for (int index = 0; index < count; ++index) {
+		off_t actual_offset;
+		bool valid = true;
+
+		if ((actual_offset = lseek(fds[index], 0, SEEK_CUR)) == -1) {
+			return -1;
+		}
+
+		// validate the ofset
+		valid = (actual_offset == expected_offset);
+		if (!valid) {
+			failed++;
+		}
+
+		// print the offset of the file descriptor
+		printf("+ fd %d: offset=%lld [%s]\n", index, (long long)actual_offset, (valid ? "VALID" : "INVALID!"));
+	}
+
+	printf("Total: %ld, failed: %d\n\n", count, failed);
+
+	return failed;
+}
+
+/**
  * Moves the file descriptor of a file descriptor and check that all move.
  */
-static int move_and_check(int fds[], size_t count, int selected, off_t offset, int whence)
+static int lseek_and_check(int fds[], size_t count, int selected, off_t offset, int whence)
 {
+	off_t new_offset;
+
 	// move the offset of the selected file descriptor
-	off_t new_offset = lseek(fds[selected], offset, whence);
+   	if ((new_offset = lseek(fds[selected], offset, whence)) == -1) {
+		return -1;
+	}
 
 	// print information about the seek operation
 	printf("lseek(fds[%d], %lld, %s) = %lld\n",
 			selected, (long long)offset, whence_to_string(whence), (long long)new_offset);
 
-	for (int index = 0; index < count; ++index) {
-		off_t actual_offset;
+	// check and print the offsets of all file descriptors
+	return check_offsets(fds, count, new_offset);
+}
 
-		// validate the new offset
-		if ((actual_offset = lseek(fds[index], 0, SEEK_CUR)) == -1) {
-			return errno;
-		}
-		assert(actual_offset == new_offset);
+/**
+ * Writes to a file descriptor and check that all move.
+ */
+static int write_and_check(int fds[], size_t count, int selected, const size_t size)
+{
+	// junk data with the specified size
+	char data[size];
+	off_t new_offset;
 
-		printf("fd %d: offset=%lld\n", index, (long long)actual_offset);
+	// write the junk data
+	if (write(fds[selected], data, size) != size) {
+		return -1;
 	}
-	
-	// success
-	return 0;
+
+	// get the new offset of the selected file descriptor
+   	if ((new_offset = lseek(fds[selected], 0, SEEK_CUR)) == -1) {
+		return -1;
+	}
+
+	// print information about the write operation
+	printf("write(fds[%d], data, %ld) = %ld\n", selected, size, size);
+
+	// check and print the offsets of all file descriptors
+	return check_offsets(fds, count, new_offset);
+}
+
+/**
+ * Reads from a file descriptor and check that all move.
+ */
+static int read_and_check(int fds[], size_t count, int selected, const size_t size)
+{
+	// data buffer with the specified size
+	char data[size];
+	off_t new_offset;
+
+	// write the data
+	if (read(fds[selected], data, size) != size) {
+		return errno;
+	}
+
+	// get the new offset of the selected file descriptor
+   	if ((new_offset = lseek(fds[selected], 0, SEEK_CUR)) == -1) {
+		return errno;
+	}
+
+	// print information about the write operation
+	printf("read(fds[%d], data, %ld) = %ld\n", selected, size, size);
+
+	// check and print the offsets of all file descriptors
+	return check_offsets(fds, count, new_offset);
 }
 
 int main(int argc, char * argv[])
@@ -137,13 +213,38 @@ int main(int argc, char * argv[])
 	// create inital file and duplicate file descriptors
 	if (setup_fds(fds, NUM_FDS) != 0) {
 		perror("Setting up file descriptors");
-		exit(EXIT_FAILURE);
+		goto fail;
 	}
 
-	move_and_check(fds, NUM_FDS, 1, 10, SEEK_SET);
+	// check offset after calling lseek, write and read for different file descriptors
+	if (lseek_and_check(fds, NUM_FDS, 1, 10, SEEK_SET) == -1) {
+		perror("Running checks for lseek");
+		goto fail;
+	}
+	if (lseek_and_check(fds, NUM_FDS, 2, -5, SEEK_CUR) == -1) {
+		perror("Running checks for lseek");
+		goto fail;
+	}
+	if (write_and_check(fds, NUM_FDS, 3, 20) == -1) {
+		perror("Running checks for write");
+		goto fail;
+	}
+	if (lseek_and_check(fds, NUM_FDS, 0, -8, SEEK_END) == -1) {
+		perror("Running checks for lseek");
+		goto fail;
+	}
+	if (read_and_check(fds, NUM_FDS, 1, 4) == -1) {
+		perror("Running checks for read");
+		goto fail;
+	}
 
 	// clean up
 	close_fds(fds, NUM_FDS);
 
 	return EXIT_SUCCESS;
+
+fail:
+	// clean up
+	close_fds(fds, NUM_FDS);
+	return EXIT_FAILURE;
 }

bd58441e772ab32c1834e9163c91bad9ddfc878e
Emil Nilsson <eonilsson@gmail.com>
2014-10-19 15:00:52 +0000

    Allow allocating large blocks of memory in 7-2

--- ch07/7-2/newalloc.c
+++ ch07/7-2/newalloc.c
@@ -4,14 +4,82 @@
 #include <stdlib.h>
 // for sbrk
 #include <unistd.h>
+// for intptr_t
+#include <stdint.h>
 // for ptrdiff_t
 #include <stddef.h>
 // for assert
 #include <assert.h>
-// for true, false
+// for bool, true, false
 #include <stdbool.h>
 // for printf
 #include <stdio.h>
+
+/**
+ * A free or allocated memory node.
+ */
+typedef struct __attribute__((__aligned__(16))) na_node {
+	/** The data capacity of the node. */
+	size_t len;
+
+	/** Whether the node is allocated. */
+	bool used;
+
+	/** The previous node in the list. */
+	struct na_node * prev;
+
+	/** The next node in the list. */
+	struct na_node * next;
+} na_node;
+
+/**
+ * Aligns a pointer "rounding" up.
+ *
+ * Alignment should be a power of 2.
+ */
+#define NA_ALIGN_PTR(ptr, type, align) ((type)(((intptr_t)ptr + ((1 << align) - 1)) & (~0 << align)))
+
+/**
+ * Aligns a size "rounding" up.
+ */
+#define NA_ALIGN_SIZE_T(size, align) ((size_t)((size + ((1 << align) - 1)) & (~0 << align)))
+
+/**
+ * Checks alignment on a pointer.
+ *
+ * Alignment should be a power of 2.
+ */
+#define NA_CHECK_ALIGN_PTR(ptr, align) (!((intptr_t)ptr & ~(~0 << align)))
+
+/**
+ * Gets the data for an allocated memory node.
+ */
+#define NA_GET_DATA_PTR(node) ((void *)node + sizeof(na_node))
+
+/**
+ * Gets the allocated memory node for its data pointer.
+ */
+#define NA_GET_NODE_PTR(data) ((void *)data - sizeof(na_node))
+
+/**
+ * Gets the total size of a memory node.
+ */
+#define NA_GET_NODE_SIZE(node) NA_GET_NODE_SIZE_FOR_LEN(node->len)
+
+/**
+ * Gets the total size of a memory node with a specific length.
+ */
+#define NA_GET_NODE_SIZE_FOR_LEN(len) (sizeof(na_node) + len)
+
+/**
+ * Gets the end of a node.
+ */
+#define NA_GET_NODE_END(node) ((void *)node + NA_GET_NODE_SIZE(node))
+
+/**
+ * The smallest required size of a node.
+ */
+#define NA_MIN_NODE_SIZE (sizeof(na_node) + NA_MIN_DATA_SIZE)
 
 /**
  * Alignment of memory nodes.
@@ -24,14 +92,26 @@
 #define NA_MIN_DATA_SIZE (1 << 4)
 
 /**
- * The smallest required size of a node.
+ * The default alignment of memory to reserve for dynamic allocation.
  */
-#define NA_MIN_NODE_SIZE (sizeof(na_node) + NA_MIN_DATA_SIZE)
+#define NA_DEFAULT_RESERVE_ALIGNMENT 12 /* 1<<12 == 4096 */
 
 /**
- * The default size of memory to reserve for dynamic allocation
+ * Reserves memory and creates a memory node.
  */
-#define NA_DEFAULT_RESERVE_MEM_SIZE (1 << 12)
+static na_node * na_reserve_node_mem(size_t size);
+
+/**
+ * Initializes a memory node.
+ *
+ * size is the total available size for the node and its data.
+ */
+static na_node * na_init_node(void * position, size_t size);
+
+/**
+ * Reserves memory for dynamic allocation.
+ */
+static void * na_reserve_mem(size_t size);
 
 /**
  * The first memory node.
@@ -46,7 +126,7 @@ static na_node * na_append_node(na_node * node);
 /**
  * Splits an unused node and returns the first half.
  */
-static na_node * na_split_node(na_node * node, size_t size);
+static na_node * na_split_node(na_node * node, size_t len);
 
 /**
  * Joins an unused node with its successor.
@@ -59,28 +139,50 @@ static na_node * na_join_node(na_node * node);
 static na_node * na_find_fitting_node(size_t size);
 
 /**
+ * Checks whether a node is a candidate for splitting.
+ */
+static bool na_should_split(na_node * node, size_t required_size);
+
+/**
  * Finds the last node.
  */
 static na_node * na_get_last_node(void);
 
-void * na_malloc(size_t size)
+void * na_malloc(size_t len)
 {
-	// find a node that fits
-	na_node * fitting_node = na_find_fitting_node(size);
+	// the size required for the new node
+	size_t size = NA_GET_NODE_SIZE_FOR_LEN(len);
+
+	// find a node that fits the lenght
+	na_node * fitting_node = na_find_fitting_node(len);
+
 	// reserve more space if no node was found
 	if (fitting_node == NULL) {
-		na_node * reserved_node = na_reserve_node_mem(NA_DEFAULT_RESERVE_MEM_SIZE);
+		size_t required_size = size;
+		na_node * last_node = na_get_last_node();
+		// should join with the last unused node?
+		bool join_last = last_node != NULL && !last_node->used;
+
+		if (join_last) {
+			// account for space in node at the end
+			required_size -= NA_GET_NODE_SIZE(last_node);
+		}
+
+		na_node * reserved_node = na_reserve_node_mem(NA_ALIGN_SIZE_T(required_size, NA_DEFAULT_RESERVE_ALIGNMENT));
 		if (reserved_node == NULL) {
 			// failed to reserve node
 			return NULL;
 		}
-		// FIXME: won't work if size > NA_DEFAULT_RESERVE_MEM_SIZE
 		fitting_node = na_append_node(reserved_node);
+		if (join_last) {
+			fitting_node = na_join_node(last_node);
+		}
+		assert(fitting_node->len >= len);
 	}
 
-	if (NA_GET_NODE_SIZE(fitting_node) >= (NA_MIN_NODE_SIZE << 1)) {
-		// the fitting node is big enough to split
-		fitting_node = na_split_node(fitting_node, size);
+	// check if the fitting node can be split into two
+	if (na_should_split(fitting_node, size)) {
+		fitting_node = na_split_node(fitting_node, len);
 		if (fitting_node == NULL) {
 			// failed to split node
 			return NULL;
@@ -163,7 +265,7 @@ na_node * na_init_node(void * position, size_t size)
 void * na_reserve_mem(size_t size)
 {
 	// check alignment
-	// WARNING: not thread safe, another thread can program break meanwhile!
+	// WARNING: not thread safe, another thread can change program break meanwhile!
 	void * current = sbrk(0);
 	if ((intptr_t)current == -1) {
 		goto fail;
@@ -222,14 +324,14 @@ void na_list_nodes(void)
 	}
 }
 
-na_node * na_split_node(na_node * node, size_t size)
+na_node * na_split_node(na_node * node, size_t len)
 {
 	assert(!node->used);
 
 	// size of the old node
-	size_t old_size = node->len + sizeof(na_node);
+	size_t old_size = NA_GET_NODE_SIZE(node);
 	// size of the left and right nodes
-	size_t left_size = NA_ALIGN_SIZE_T(size + sizeof(na_node), NA_NODE_ALIGNMENT);
+	size_t left_size = NA_ALIGN_SIZE_T(NA_GET_NODE_SIZE_FOR_LEN(len), NA_NODE_ALIGNMENT);
 	size_t right_size = old_size - left_size;
 
 	// check that the sizes is adequate
@@ -290,16 +392,23 @@ na_node * na_append_node(na_node * node)
 	return node;
 }
 
-na_node * na_find_fitting_node(size_t size)
+na_node * na_find_fitting_node(size_t len)
 {
 	for (na_node * node = na_first_node; node; node = node->next) {
-		if (!node->used && node->len >= size) {
+		if (!node->used && node->len >= len) {
 			return node;
 		}
 	}
 
 	// none found
 	return NULL;
+}
+
+bool na_should_split(na_node * node, size_t required_size)
+{
+	// check the remaining size
+	size_t available_size = NA_GET_NODE_SIZE(node) - NA_ALIGN_SIZE_T(required_size, NA_NODE_ALIGNMENT);
+	return available_size >= NA_MIN_NODE_SIZE;
 }
 
 na_node * na_get_last_node(void)
--- ch07/7-2/newalloc.h
+++ ch07/7-2/newalloc.h
@@ -3,93 +3,16 @@
 
 // for size_t
 #include <stdlib.h>
-// for bool
-#include <stdbool.h>
-// for intptr_t
-#include <stdint.h>
-
-/**
- * A free or allocated memory node.
- */
-typedef struct __attribute__((__aligned__(16))) na_node {
-	/** The data capacity of the node. */
-	size_t len;
-
-	/** Whether the node is allocated. */
-	bool used;
-
-	/** The previous node in the list. */
-	struct na_node * prev;
-
-	/** The next node in the list. */
-	struct na_node * next;
-} na_node;
-
-/**
- * Aligns a pointer "rounding" up.
- *
- * Alignment should be a power of 2.
- */
-#define NA_ALIGN_PTR(ptr, type, align) ((type)(((intptr_t)ptr + ((1 << align) - 1)) & (~0 << align)))
-
-/**
- * Aligns a size "rounding" up.
- */
-#define NA_ALIGN_SIZE_T(size, align) ((size_t)((size + ((1 << align) - 1)) & (~0 << align)))
-
-/**
- * Checks alignment on a pointer.
- *
- * Alignment should be a power of 2.
- */
-#define NA_CHECK_ALIGN_PTR(ptr, align) (!((intptr_t)ptr & ~(~0 << align)))
-
-/**
- * Gets the data for an allocated memory node.
- */
-#define NA_GET_DATA_PTR(node) ((void *)node + sizeof(na_node))
-
-/**
- * Gets the allocated memory node for its data pointer.
- */
-#define NA_GET_NODE_PTR(data) ((void *)data - sizeof(na_node))
-
-/**
- * Gets the total size of a memory node.
- */
-#define NA_GET_NODE_SIZE(node) (sizeof(na_node) + node->len)
-
-/**
- * Gets the end of a node.
- */
-#define NA_GET_NODE_END(node) ((void *)node + NA_GET_NODE_SIZE(node))
 
 /**
  * Allocates memory on the heap.
  */
-extern void * na_malloc(size_t size);
+extern void * na_malloc(size_t len);
 
 /**
  * Frees memory allocated on the heap.
  */
 extern void na_free(void * data);
-
-/**
- * Reserves memory and creates a memory node.
- */
-extern na_node * na_reserve_node_mem(size_t size);
-
-/**
- * Initializes a memory node.
- *
- * size is the total available size for the node and its data.
- */
-extern na_node * na_init_node(void * position, size_t size);
-
-/**
- * Reserves memory for dynamic allocation.
- */
-extern void * na_reserve_mem(size_t size);
 
 /**
  * Prints information about the memory nodes.
@@ -100,7 +23,6 @@ extern void na_print_nodes(void);
  * Prints list of used memory nodes.
  */
 extern void na_list_nodes(void);
-
 
 #endif
 
